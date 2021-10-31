0e40772383d61506b056aa1a42d8f0131a6b1f04
manchann <chl8273@naver.com>
2021-11-01 01:50:51 +0900

    acc test

--- ensemble/lambda_function.py
+++ ensemble/lambda_function.py
@@ -82,7 +82,7 @@ def lambda_handler(event, context):
     for single_result in result:
         single_result = [(img_class, label, round(acc * 100, 4)) for img_class, label, acc in single_result]
         results += single_result
-        pred_labels = [label for img_class, label, acc in single_result]
+        pred_labels = [img_class for img_class, label, acc in single_result]
 
     acc = np.sum(np.array(actual_labels) == np.array(pred_labels)) / len(actual_labels)
 
