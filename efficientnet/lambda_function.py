81c0d3b3c6769ad85b78725c078eb04186a4e004
manchann <chl8273@naver.com>
2021-10-31 17:48:39 +0900

    efficientnet b1 -> b0 changed

--- efficientnet/lambda_function.py
+++ efficientnet/lambda_function.py
@@ -8,7 +8,7 @@ import time
 
 bucket_name = 'imagenet-sample'
 bucket_ensemble = 'lambda-ensemble'
-model_name = 'efficientnetb1'
+model_name = 'efficientnetb0'
 model_path = '/var/task/lambda-ensemble-sequence/efficientnet/model/' + model_name
 model = load_model(model_path, compile=True)
 
@@ -55,7 +55,7 @@ def filenames_to_input(file_list):
     imgs = []
     for file in file_list:
         img = read_image_from_s3(file)
-        img = img.resize((240, 240), Image.ANTIALIAS)
+        img = img.resize((224, 224), Image.ANTIALIAS)
         img = np.array(img)
         # if image is grayscale, convert to 3 channels
         if len(img.shape) != 3:
